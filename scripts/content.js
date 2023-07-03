44d7e87ab6d1def021d4ef4298a7db465213456d
Georgehu716 <2992786775@qq.com>
2023-07-03 11:32:52 +0800

    Add basic search bar

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,19 @@
 console.log("Hi, GitHub following search bar.")
+
+const userProfileFrame = document.getElementById("user-profile-frame")
+const searchDiv= document.createElement("div")
+searchDiv.id = "new-search-div"
+// searchDiv.textContent = `Hello, I am a new created p`
+
+function getSearchForm() {
+    return `
+    <form data-pjax="true" data-turbo="true" data-turbo-frame="user-starred-repos" data-turbo-action="advance" class="subnav-search ml-0 mb-1 mb-lg-0 mr-lg-3 flex-auto width-full" action="" accept-charset="UTF-8" method="get">
+        <input type="search" name="q" value="" class="form-control width-full subnav-search-input" placeholder="Search following" aria-label="Search following" autocapitalize="off" autocomplete="off">
+        <button type="submit" data-view-component="true" class="sr-only btn>Search</button>
+    </form>
+    `
+}
+
+searchDiv.innerHTML = getSearchForm()
+
+userProfileFrame.insertAdjacentElement("beforebegin", searchDiv)
