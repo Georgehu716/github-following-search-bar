669eec43ee546a9abb1cf4c7139b92b535d21896
Georgehu716 <2992786775@qq.com>
2023-07-04 16:24:02 +0800

    Remove question and update search form

--- scripts/content.js
+++ scripts/content.js
@@ -7,13 +7,20 @@ searchDiv.id = "new-search-div"
 
 function getSearchForm() {
     return `
-    <form data-pjax="true" data-turbo="true" data-turbo-frame="user-starred-repos" data-turbo-action="advance" class="subnav-search ml-0 mb-1 mb-lg-0 mr-lg-3 flex-auto width-full" action="" accept-charset="UTF-8" method="get">
-        <input type="search" name="q" value="" class="form-control width-full subnav-search-input" placeholder="Search following" aria-label="Search following" autocapitalize="off" autocomplete="off">
+    <form id="new-search-form" data-pjax="true" data-turbo="true" data-turbo-frame="user-starred-repos" data-turbo-action="advance" class="subnav-search ml-0 mb-1 mb-lg-0 mr-lg-3 flex-auto width-full" action="" accept-charset="UTF-8" method="get">
+        <input id="new-search-input" type="search" name="q" value="" class="form-control width-full subnav-search-input" placeholder="Search following" aria-label="Search following" autocapitalize="off" autocomplete="off">
         <button type="submit" data-view-component="true" class="sr-only btn>Search</button>
     </form>
     `
 }
 
 searchDiv.innerHTML = getSearchForm()
-
 userProfileFrame.insertAdjacentElement("beforebegin", searchDiv)
+
+const newSearchForm = document.getElementById("new-search-form")
+newSearchForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const newSearchInput = document.getElementById("new-search-input")
+    console.log("new search input: ", newSearchInput.value)
+})
